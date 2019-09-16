d823361fd5ae7dbdd9e0d6ebbf56afe96f0e589a
Konstantin <zubr133@gmail.com>
2019-09-16 18:30:10 +0300

    Updated gallery UI

--- PuzzlePublisher.sketchplugin/Contents/Sketch/viewer/gallery.js
+++ PuzzlePublisher.sketchplugin/Contents/Sketch/viewer/gallery.js
@@ -4,20 +4,22 @@ function createGallery() {
         initialize: function()   {
         },
         show: function() {
-            $('#gallery').removeClass('hidden');
+            $('#gallery-modal').removeClass('hidden');
             if(!this.loaded){
                 this.loadPages();
                 this.loaded = true;
+                //load amount of pages to gallery title
+                document.getElementById("screensamount").innerHTML = viewer.userStoryPages.length + " screens";
             }
         },
         hide: function(){
-            $('#gallery').addClass('hidden');
+            $('#gallery-modal').addClass('hidden');
         },
         isVisible: function(){
-            return ! $('#gallery').hasClass('hidden')
+            return ! $('#gallery-modal').hasClass('hidden')
         },
         toogle: function(){
-            if ( $('#gallery').hasClass('hidden')) {
+            if ( $('#gallery-modal').hasClass('hidden')) {
                 gallery.show();
             } else {
                 gallery.hide();
@@ -33,34 +35,41 @@ function createGallery() {
             viewer.goToPage(index);
         },
         loadOnePage: function(page){
-            var imageURI = story.hasRetina && viewer.isHighDensityDisplay() ? page.image2x : page.image;	
-            
-            var width = 300;
-            var height = Math.round(page.height / (page.width / width));
+            var imageURI = story.hasRetina && viewer.isHighDensityDisplay() ? page.image2x : page.image;
 
             var div = $('<div/>', {
                 id : page.index,
-                class: "gallery-div",                             
+                class: "grid-cell",                             
+            });
+
+            var divWrapper = $('<div/>', {
+                class: "grid-cell-wrapper"
+            });
+            var divMain = $('<div/>', {
+                class: "grid-cell-main"
             });
             div.click(function(e){                
                 gallery.selectPage(parseInt(this.id));
             });
-            div.appendTo($('#gallery'));
+            div.appendTo($('#grid'));
 
             var img = $('<img/>', {
                 id : "img_gallery_"+ page.index,
                 class: "gallery-image",
                 alt: page.title,
 				src : encodeURIComponent(viewer.files) + '/previews/' + encodeURIComponent(imageURI),
-            }).attr('width', width).attr('height', height);
-            img.appendTo(div);
+            });
+
+            img.appendTo(divMain);
+            divMain.appendTo(divWrapper);
+            divWrapper.appendTo(div);
 
             var title = $('<span/>', {
                 id: "page-title",
                 alt: page.title,
                 text: page.title,
             });
-            title.appendTo(div);
+            title.appendTo(divMain);
         }
     }
 }
