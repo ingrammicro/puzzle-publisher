4bdf67e6e4bea4454885ebd6d399da64712822a1
Max Bazarov <max@bazarov.ru>
2020-02-14 13:41:16 +0300

    Corrected example

--- PuzzlePublisher.sketchplugin/Contents/Sketch/cmdline-functions.js
+++ PuzzlePublisher.sketchplugin/Contents/Sketch/cmdline-functions.js
@@ -2,7 +2,7 @@
 
 // osascript -e 'quit app "Sketch"'
 const example = `
-/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool --without-activating=YES --new-instance=No run ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/Exporter.sketchplugin "cmdRun"  --context='{"file":"/Users/baza/GitHub/exporter/tests/Links2.sketch","commands":"sync,export"}'
+/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool --without-activating=YES --new-instance=No run ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/PuzzlePublisher.sketchplugin "cmdRun"  --context='{"file":"/Users/baza/GitHub/puzzle-publisher/tests/Links2.sketch","commands":"sync,export,publish,save"}'
 
 `
 
