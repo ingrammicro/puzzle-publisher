cb69cd6cb283abea06f73050be46a991a1350036
Maxim Bazarov <maxim.bazarov@ingrammicro.com>
2020-05-25 22:45:45 +0300

    Puzzle Tokens shares it wit PP

---
+++ PuzzlePublisher.sketchplugin/Contents/Sketch/lib/ga.js
@@ -0,0 +1,71 @@
+@import "lib/utils.js";
+@import "constants.js";
+
+function jsonToQueryString(json) {
+    return Object.keys(json)
+        .map(function (key) {
+            return encodeURIComponent(key) + "=" + encodeURIComponent(json[key]);
+        })
+        .join("&");
+}
+
+
+
+function track(page, props = undefined) {
+    coscript.scheduleWithInterval_jsFunction(1, function () {
+
+        var trackingId = "UA-84277242-4"
+
+        var Settings = require("sketch/settings");
+        var kUUIDKey = "google.analytics.uuid";
+        var uuid = null
+        var uuid = NSUserDefaults.standardUserDefaults().objectForKey(kUUIDKey);
+        if (!uuid) {
+            uuid = NSUUID.UUID().UUIDString();
+            NSUserDefaults.standardUserDefaults().setObject_forKey(uuid, kUUIDKey)
+        }
+
+        var variant = MSApplicationMetadata.metadata().variant;
+        var source =
+            "Sketch " +
+            (variant == "NONAPPSTORE" ? "" : variant + " ") +
+            Settings.version.sketch;
+
+
+        if (Settings.settingForKey(SettingKeys.PLUGIN_GA_DISABLED)) {
+            // the user didn't enable sharing analytics
+            return 'the user didn\'t enable sharing analytics';
+        }
+
+        var payload = {
+            v: 1,
+            tid: trackingId,
+            ds: source,
+            cid: uuid,
+            t: "pageview",
+            dp: page
+        };
+
+        if (typeof __command !== "undefined") {
+            payload.an = __command.pluginBundle().name();
+            payload.aid = __command.pluginBundle().identifier();
+            payload.av = __command.pluginBundle().version();
+        }
+
+        if (props) {
+            Object.keys(props).forEach(function (key) {
+                payload[key] = props[key];
+            });
+        }
+
+        var nURL = NSURL.URLWithString(
+            "https://www.google-analytics.com/collect?payload_data&" +
+            jsonToQueryString(payload) +
+            "&z=" +
+            NSUUID.UUID().UUIDString()
+        );
+        if (DEBUG) log("Started GA " + nURL)
+        NSData.dataWithContentsOfURL(nURL);
+        if (DEBUG) log("Completed GA")
+    })
+}
