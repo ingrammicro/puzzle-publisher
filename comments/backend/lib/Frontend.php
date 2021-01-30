3b92f0848f15f0a5a60fd64e3c4df7f1c80cac08
Max Bazarov <mbazarov@gmail.com>
2021-01-30 23:24:14 +0300

    Refactored forms

--- comments/backend/lib/Frontend.php
+++ comments/backend/lib/Frontend.php
@@ -5,8 +5,111 @@ class Frontend
     public static function buildFullHTML(&$forum,&$page,&$commentsInfo){
         $url =  ($_SERVER['HTTPS']=="on"?"https://":"http://").$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT'].$_SERVER['SCRIPT_NAME'];
         //var_dump($_SERVER);
+        $inputStyle=' style="font-size:12px;"' ;
         $res = <<<EOL
+        <div id="top"/>
         <script>    
+            class CommentsAbstractForm{
+                constructor(formName){                    
+                    this.formName = formName
+                    this.built = false
+                }
+                _tuneInput(input){
+                    input.focusin(function () {
+                        comments.inputFocused = true
+                    })
+                    input.focusout(function () {
+                        comments.inputFocused = false
+                    })
+                }
+                showError(errorText) {
+                    $("#comments_viewer #"+this.formName + "#error").html(errorText);
+                }
+                show(){
+                    if(!this.built) this.buildHTML();
+                    $("#comments_viewer #"+this.formName).show()
+                }
+                hide(){
+                    $("#comments_viewer #"+this.formName).hide()
+                }
+                //// to overwrite
+                buildHTML(){
+                    this.built = true
+                }
+                getDataFromForm(){
+                }
+                putDataInForm(){
+                }
+                checkData(){
+                }
+                handleEnterKey(){
+                    return false
+                }    
+                submit(){
+                }            
+            }
+            class CommentsLoginForm extends CommentsAbstractForm{
+                constructor(){
+                    super("loginForm")
+                    this.email = ""                                        
+                }
+                putDataInForm(){
+                    let emailField = $("#comments_viewer #loginForm #email")
+                    emailField.val(this.email);
+                    this._tuneInput(emailField)    
+                }
+                // Check data
+                checkData(){
+                    if(""==this.email){
+                        this.showError("Specify email");
+                        return false;
+                    }
+                    return true
+                }  
+                getDataFromForm(){
+                    this.email = $("#comments_viewer #loginForm #email").val();              
+                }
+                buildHTML(){
+                    super.buildHTML()
+                    let s=`
+        <div id='loginForm'>
+            <div id="title" style="font-weight:bold;">Login As</div>
+            <div id="error" style="color:red">
+            </div>
+            <div>
+                <input id="email" {$inputStyle} placeholder="Your email"/>
+            </div>
+            <div id="buttons">
+                <input id="send" type="button" onclick="comments.loginForm.submit();return false;" value="Login"/>
+            </div>
+        </div>`
+                    $("#comments_viewer #top").html(s);
+                }
+                submit(){
+                    this.getDataFromForm();                    
+                    if(!this.checkData()) return false;
+                    ///
+                    var formData = new FormData();
+                    formData.append("email",  this.email);                  
+                    //
+                    var handler =function () {
+                        var result =  JSON.parse(this.responseText);
+                        //                        
+                        console.log(this.responseText); 
+                        if(result.status!='ok'){
+                            this.showError(result.message);
+                        }else{
+                            console.log(result); 
+                            this.hide()
+                            comments.authForm.show()
+                        }
+                    }                
+                    //    
+                    return comments.sendCommand("login", formData,handler);
+                }
+
+            }
+            ////
             class Comments {
                 constructor() {
                     this.forumID = "{$forum->forumID()}"
@@ -19,19 +122,12 @@ class Frontend
                         email:"",
                         msg:"" 
                     }                
+                    //
+                    this.loginForm =new CommentsLoginForm()
+                    this.authForm = null
+                    //
                     this.inputFocused = false                                       
-                }
-                _tuneInput(input){
-                    input.focusin(function () {
-                        comments.inputFocused = true
-                    })
-                    input.focusout(function () {
-                        comments.inputFocused = false
-                    })
-                }
-                showError(errorText) {
-                    $("#comments_viewer #commentForm #error").html(errorText);
-                }
+                }                        
                 sendCommand(cmd,formData,handler) {
                     var xhr = new XMLHttpRequest()                    
                     xhr.open('POST',this.url + "?fid="+this.forumID+"&cmd="+cmd, true)
@@ -88,7 +184,7 @@ class Frontend
                         //                        
                         console.log(this.responseText); 
                         if(result.status!='ok'){
-                            comments.showError(result.message);
+                            this.showError(result.message);
                         }else{
                             console.log(result); 
                             $("#comments_viewer #comments").html(result.data);
@@ -105,7 +201,7 @@ class Frontend
                         var result =  JSON.parse(this.responseText);
                         //
                         if(result.status!='ok'){
-                            comments.showError(result.message);
+                            this.showError(result.message);
                         }else{
                             $("#comments_viewer #comments").html(result.data);
                             comments.comment.msg = ""
@@ -116,45 +212,8 @@ class Frontend
                     var formData = new FormData();
                     return comments.sendCommand("buildCommentsHTML", formData,handler);
                 }
-                ////////
-                fillLoginForm(){
-                    let emailField = $("#comments_viewer #loginForm #email")
-                    emailField.val(this.loginForm.email);
-                    this._tuneInput(emailField)    
-                }
-                // Check data
-                checkLoginForm(){
-                    if(""==this.loginForm.email){
-                        this.showError("Specify email");
-                        return false;
-                    }
-                    return true
-                }  
-                readLoginForm(){
-                    this.loginForm.email = $("#comments_viewer #loginForm #email").val();              
-                }
-                submitLoginForm(){
-                    this.readLoginForm();                    
-                    if(!this.checkLoginForm()) return false;
-                    ///
-                    var formData = new FormData();
-                    formData.append("email",  this.loginForm.email);                  
-                    //
-                    var handler =function () {
-                        var result =  JSON.parse(this.responseText);
-                        //                        
-                        console.log(this.responseText); 
-                        if(result.status!='ok'){
-                            comments.showError(result.message);
-                        }else{
-                            console.log(result); 
-                            $("#comments_viewer #loginForm").hide()
-                            $("#comments_viewer #authForm").show()
-                        }
-                    }                
-                    //    
-                    return comments.sendCommand("login", formData,handler);
-                }
+                ////////s            
+               
                 ////////////////////////// 
                 // Check data
                 readAuthForm(){
@@ -179,7 +238,7 @@ class Frontend
                         //                        
                         console.log(this.responseText); 
                         if(result.status!='ok'){
-                            comments.showError(result.message);
+                            this.showError(result.message);
                         }else{
                             console.log(result); 
                             $("#comments_viewer #authForm").hide()
@@ -189,16 +248,17 @@ class Frontend
                     //    
                     return comments.sendCommand("checkAuth", formData,handler);
                 }
+                run(){
+                    if(this.uid!=""){
+                    }else{
+                        this.loginForm.show()
+                    }
+                }
             }
             comments = new Comments();
-        </script>
+            comments.run()
+        </script>        
 EOL;
-        //        
-        if(""!=$forum->uid){
-            $res .= Frontend::buildCommentFormHTML($page);
-        }else{
-            $res .= Frontend::buildLoginHTML($page);
-        }
         $res .= "<div id='comments'>" . Frontend::buildCommentListHTML($page,$commentsInfo)."</div>";
         return $res;
     }
