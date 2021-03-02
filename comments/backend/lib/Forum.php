618140fc66d655fbe79e6c4398f5dc38c35bb175
Maxim Bazarov <maxim.bazarov@ingrammicro.com>
2021-03-02 13:40:21 +0300

    Update Forum.php

--- comments/backend/lib/Forum.php
+++ comments/backend/lib/Forum.php
@@ -82,6 +82,18 @@ const DEF_PAGE_INFO = [
     "comments"=>[],
 ];
 
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+    $headers = [];
+    foreach ($_SERVER as $name => $value) {
+        if (substr($name, 0, 5) == 'HTTP_') {
+            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+    }
+    return $headers;
+    }
+}
+
 class Page
 {
     private $info =  [];
