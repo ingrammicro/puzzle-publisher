6bb1b13764721793eb2521988b30c4439d049285
Maxim Bazarov <Maxim.Bazarov@ingrammicro.com>
2021-04-09 13:39:26 +0300

    Update export.sh

--- PuzzlePublisher.sketchplugin/Contents/Sketch/scripts/export.sh
+++ PuzzlePublisher.sketchplugin/Contents/Sketch/scripts/export.sh
@@ -4,11 +4,11 @@ tempDir="$1"
 fileName="$2"
 docName="$3"
 srcSketchPath="/Applications/Sketch.app"
-dstSketchPath="${tempDir}/SketchExport.app"
+#dstSketchPath="${tempDir}/SketchExport.app"
 #rm -rf $dstSketchPath
 #cp -R $srcSketchPath $dstSketchPath
 #echo $dstSketchPath
-dstSketchPath = $srcSketchPath
-${dstSketchPath}/Contents/Resources/sketchtool/bin/sketchtool --application=SketchExport--new-instance=NO --wait-for-exit=NO --without-waiting=NO --without-activating=YES run ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/PuzzlePublisher.sketchplugin "cmdRun"  --context="{\"file\":\"${fileName}\",\"name\":\"${docName}\",\"commands\":\"export,close\",\"async\":true}"
+dstSketchPath=$srcSketchPath
+${dstSketchPath}/Contents/Resources/sketchtool/bin/sketchtool --new-instance=YES --wait-for-exit=NO --without-waiting=NO --without-activating=YES run ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/PuzzlePublisher.sketchplugin "cmdRun"  --context="{\"file\":\"${fileName}\",\"name\":\"${docName}\",\"commands\":\"export,close\",\"async\":true}"
 rm $fileName
 kill $(ps -Ac -o pid,comm | awk '/^ *[0-9]+ Sketch$/ {print $1}' | tail -n 1)
