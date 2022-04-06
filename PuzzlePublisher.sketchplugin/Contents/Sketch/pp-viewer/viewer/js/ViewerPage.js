6317dd36f846c5c6851057b625085005c9427e66
Maxim Bazarov <Maxim.Bazarov@ingrammicro.com>
2022-04-06 17:27:04 +0300

    Support for Cmd+click

--- PuzzlePublisher.sketchplugin/Contents/Sketch/pp-viewer/viewer/js/ViewerPage.js
+++ PuzzlePublisher.sketchplugin/Contents/Sketch/pp-viewer/viewer/js/ViewerPage.js
@@ -1027,7 +1027,7 @@ function handleLinkEvent(event) {
     } else if (link.url != null) {
         //title = link.url;
         viewer.currentPage.hideCurrentOverlays()
-        var target = link.target
+        var target = event.metaKey ? "_blank" : link.target
         window.open(link.url, target != undefined ? target : "_self")
         return false
         //document.location = link_url
